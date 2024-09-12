0cc01863db683ca24d00b9d6fc486797e44d6fd7
kojoaning <aningabankwa@gmail.com>
2024-09-12 19:34:29 +0000

    added requirements.txt

--- intellidiag/settings.py
+++ intellidiag/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9b!8%zd1enf%d9(x&e!vw$l*4vw6!2fy1yzd*y!-u!c#8#*c)7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["intellidiag.onrender.com"]
 
 
 # Application definition
